141636136cdd75c1cc7b67d3508604f60abcbd08
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-03-24 22:41:44 -0700

    Connected application to Azure DB. Added unit test using mock repositories. Added Pagination. Added styling through bootstrap.

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -7,6 +7,7 @@ using SportsStore.Domain.Entities;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Ninject;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -28,14 +29,7 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product{ Name="Juan Carlos Jimenez Signature Football", Price=25},
-                new Product{ Name="Surf board", Price = 179},
-                new Product{ Name = "Running shoes", Price=95}
-            });
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
 
     }
---
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } 
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
