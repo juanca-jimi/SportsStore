8c65213b69f69ea489cc6f3c910c971e438dda98
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-05-06 08:29:02 -0700

    Made Changes to the IProductsReposistory, EFProductReposistory, and Product.cs. Added AdminTest with many administrative changes tested. Added Admin Controller and 2 views (Index & Edit) tied to it. Changed the connection string to a new connection string to make administrative changes. To Initiate the new database I used test data in queries from this project.

--- SportsStore.Domain/Abstract/IProductsRepository.cs
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Abstract
@@ -7,5 +6,7 @@ namespace SportsStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+        void SaveProduct(Product product);
+        Product DeleteProduct(int productID);
     }
 }
